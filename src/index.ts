ac8b5be095ca2e0e1526e6103bde848face0b46c
ItzLing <itz.dickson1304@gmail.com>
2026-07-12 16:55:38 +0800

    feat: add main module entrypoint, created index.ts to export all domain models, workspace managers and the application shell for external consumers

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// Shared Types & Enums
+export * from "./domain/shared";
+
+// Core Domain Entities
+export * from "./domain/Candidate";
+export * from "./domain/RoleListing";
+export * from "./domain/AIJustification";
+
+// Workspaces
+export * from "./workspaces/TalentPipelineWorkspace";
+export * from "./workspaces/ActiveMatchesWorkspace";
+export * from "./workspaces/OpenRolesWorkspace";
+export * from "./workspaces/DirectIntroductionsWorkspace";
+
+// Application Shell
+export * from "./shell/CareerOSApplicationShell";
